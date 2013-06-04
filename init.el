1aef658e04191eef176992a41e1b02ab5b63c11c
tomoyuki@CentOS6X64 <hzd02765@nifty.com>
2013-06-04 17:06:17 +0900

    add plugin

---
+++ init.el
@@ -0,0 +1,31 @@
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;; ------------------------------------------------------------------------
+;; @ load-path
+
+;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; load-pathに追加するフォルダ
+;; 2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
+(add-to-load-path "elisp")
+
+(require 'session)
+(add-hook 'after-init-hook 'session-initialize)
+
+;;; 言語を日本語にする
+(set-language-environment 'Japanese)
+;;; 極力UTF-8とする
+(prefer-coding-system 'utf-8)
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
+;;; 行番号を表示
+(global-linum-mode t)
+(setq linum-format "%4d ")
